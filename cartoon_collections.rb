1c448f177e55726d5471b433100d511c8fefc973
Marios Dimitropoulos <dimitropoulosmarios@hotmail.com>
2019-10-13 22:37:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,8 +10,8 @@ end
 roll_call_dwarves(dwarves)
 
 
-def summon_captain_planet# code an argument here
-  # Your code here
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+def summon_captain_planet
 end
 
 def long_planeteer_calls# code an argument here
