40d9db0e2557faaa502a1fc0ffad11259e8fe5a9
Marios Dimitropoulos <dimitropoulosmarios@hotmail.com>
2019-10-13 22:58:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -33,3 +33,12 @@ def long_planeteer_calls(array)
   end 
 end
 
+cheese_types = ["cheddar", "gouda", "camembert"]
+snacks = ["crackers", "gouda", "thyme"]
+
+def find_the_cheese(array)
+  cheese_types = ["cheddar","gouda", "camembert"]
+    array.find do |type|
+    cheese_types.include?(type)
+  end
+end
